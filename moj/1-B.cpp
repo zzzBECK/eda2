2b803fbf9421f806e168b92dc852c82b07417167
Alexandre Beck <alexandrebeck2003@gmail.com>
2025-11-18 23:55:06 -0300

    pass: accepted lista 1 B

--- moj/1-B.cpp
+++ moj/1-B.cpp
@@ -1,22 +1,54 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+int height_from(int root, const vector<vector<int>> &children) {
+    if (children[root].empty()) {
+        return 1;
+    }
+
+    int best = 0;
+    for (int v : children[root]) {
+        int hChild = height_from(v, children);
+        if (hChild > best) {
+            best = hChild;
+        }
+    }
+
+    return 1 + best;
+}
 
 int main() {
     ios::sync_with_stdio(false);
 
-    int quantity, sum = 0;
+    int quantity;
     cin >> quantity;
+
+    vector<vector<int>> children(quantity + 1);
+    vector<int> parent(quantity + 1);
 
     for (int i = 1; i <= quantity; i++) {
         int value;
         cin >> value;
+
         if (value != -1) {
-            sum++;
+            children[value].push_back(i);
+        } else {
+            parent.push_back(i);
         }
     }
 
-    cout << sum << '\n';
+    int best = -99999;
+
+    for (auto root : parent) {
+        auto height = height_from(root, children);
+        if (height > best) {
+            best = height;
+        }
+    }
+
+    cout << best << '\n';
 
     return 0;
 }
