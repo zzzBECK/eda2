4b9860bff4f909a8b634e032f44a8cb03622909c
Alexandre Beck <alexandrebeck2003@gmail.com>
2025-11-19 23:25:24 -0300

    pass: accepted lista 1 C

--- moj/1-C.cpp
+++ moj/1-C.cpp
@@ -5,36 +5,40 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int n, q;
     cin >> n >> q;
 
-    vector<vector<int>> adj_list(n + 1);
-    vector<int> output(q + 1);
+    vector<int> list(51);
 
     for (int i = 1; i <= n; i++) {
-        int input;
-        cin >> input;
-        adj_list[input].push_back(i);
+        int color;
+        cin >> color;
+
+        if (list[color] == 0) {
+            list[color] = i;
+        }
     }
 
     for (int i = 1; i <= q; i++) {
-        int input, best = 0;
-        cin >> input;
+        int color, color_idx;
+        cin >> color;
 
-        best = adj_list[input].front();
-        adj_list[input].erase(adj_list[input].begin());
-        adj_list[input].push_back(1);
+        color_idx = list[color];
 
-        adj_list[1].erase(adj_list[1].begin());
-        adj_list[1].insert(adj_list[1].begin(), best);
+        cout << color_idx << ' ';
 
-        output.push_back(best);
+        for (auto &element : list) {
+            if (element < color_idx) {
+                element++;
+            }
+        }
+
+        list[color] = 1;
     }
 
-    for (auto value : output) {
-        cout << value << ' ';
-    }
+    cout << '\n';
 
     return 0;
 }
